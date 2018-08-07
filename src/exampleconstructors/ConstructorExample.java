b0b53be19c6900f9253e77b2f359f189a8a1de83
Mosarrof Hossain <phmosharaf@gmail.com>
2018-08-07 13:37:46 -0400

    New advanced code added

--- src/exampleconstructors/ConstructorExample.java
+++ src/exampleconstructors/ConstructorExample.java
@@ -1,12 +1,20 @@
-package exampleConstructors;
+package exampleconstructors;
 
 public class ConstructorExample {
 	
+	private int id;
+	
 	public ConstructorExample() {
+		
+		this.id = 79990;
+		this.id = 7790;
 		
 	}
 
 	public static void main(String[] args) {
+		
+		ConstructorExample Obj1 = new ConstructorExample();
+		System.out.println(Obj1.id);
 		
 
 	}
