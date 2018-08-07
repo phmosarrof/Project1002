7c6635923ba9c0815b18d59c8ee73efbf70f2d3e
Mosarrof Hossain <phmosharaf@gmail.com>
2018-08-07 14:59:06 -0400

    Third correction is added

---
+++ src/exampleconstructors/Example3.java
@@ -0,0 +1,18 @@
+package exampleconstructors;
+class Example3
+{
+      private int var;
+      public Example3(int num)
+      {
+             var=num;
+      }
+      public int getValue()
+      {
+              return var;
+      }
+      public static void main(String args[])
+      {
+              Example3 myobj = new Example3(10);
+              System.out.println("value of var is: "+myobj.getValue());
+      }
+}
