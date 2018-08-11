f3e2edc53c6bccbc4ee16571d62a52ba77542d6e
Mosarrof Hossain <phmosharaf@gmail.com>
2018-08-11 17:15:26 -0400

    Arranged Newly

--- src/exampleconstructors/ChildClass2.java
+++ src/exampleconstructors/ChildClass2.java
@@ -5,7 +5,10 @@ public class ChildClass2 extends ChildClass {
 	public static void main(String[] args) {
 
 		ChildClass2 obj2 = new ChildClass2();
-		System.out.println(obj2.b);
+		
+		System.out.println();
+		System.out.println(" The Value of B :"+obj2.b);
+		System.out.println();
 		obj2.display();
 
 
