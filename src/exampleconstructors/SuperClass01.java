7b98878fd38026aa76fe655577ae7d840b4d7a7e
Mosarrof Hossain <phmosharaf@gmail.com>
2018-08-11 14:34:53 -0400

    Encap added

---
+++ src/exampleconstructors/SuperClass01.java
@@ -0,0 +1,10 @@
+package exampleconstructors;
+
+public class SuperClass01 {
+	class Superclass
+	{
+		int num = 100;
+	
+	//Child class or subclass or derived class
+}
+}
--- src/exampleconstructors/Superclass.java
+++ src/exampleconstructors/Superclass.java
@@ -1,12 +1,6 @@
 package exampleconstructors;
-//Parent class or Superclass or base class
-class Superclass
-{
-	int num = 100;
-}
-//Child class or subclass or derived class
 
-class Subclass extends Superclass
+class Subclass extends Superclass01
 {
 	/* The same variable num is declared in the Subclass
 	 * which is already present in the Superclass
