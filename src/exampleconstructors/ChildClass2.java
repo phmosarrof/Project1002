429ca29def4166f5dd577e656c50edd535f25965
Mosarrof Hossain <phmosharaf@gmail.com>
2018-08-11 17:06:38 -0400

    Inheritance

---
+++ src/exampleconstructors/ChildClass2.java
@@ -0,0 +1,14 @@
+package exampleconstructors;
+
+public class ChildClass2 extends ChildClass {
+
+	public static void main(String[] args) {
+		
+		ChildClass2 obj2 = new ChildClass2();
+		System.out.println(obj2.b);
+		obj2.display();
+		
+
+	}
+
+}
---
+++ src/exampleconstructors/Inheritance.java
@@ -0,0 +1,17 @@
+package exampleconstructors;
+
+public class Inheritance { // parent class 
+	
+	public int a = 10;
+	protected int b = 90;
+	private  int c =77;
+	
+	public void display() {
+		
+		
+		System.out.println(" method of Parent class ");
+	}
+	
+	
+
+}
