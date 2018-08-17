4ab1391c8a6d9040395a4b9934a1c20fa539afce
Mosarrof Hossain <phmosharaf@gmail.com>
2018-08-17 16:49:30 -0400

    Done

--- src/exampleconstructors/ArraySolutions.java
+++ src/exampleconstructors/ArraySolutions.java
@@ -6,10 +6,12 @@ class Student
 {
 	public int roll_no;
 	public String name;
-	Student(int roll_no, String name)
+	public int mark;
+	Student(int roll_no, String name, int mark)
 	{
 		this.roll_no = roll_no;
 		this.name = name;
+		this.mark = mark;
 	}
 }
 // Elements of array are objects of a class Student.
@@ -18,19 +20,19 @@ public class ArraySolutions
 {
 	public static void main (String[] args)
 	{
-		//Student[] arr; 							 // declares an Array of integers.
-		// arr = new Student[5]; 					 // allocating memory for 5 objects of type Student.
+		//Student[] arr; 							 			// declares an Array of integers.
+		// arr = new Student[5]; 					 			// allocating memory for 5 objects of type Student.
 
 		Student [] arr = new Student[6];			
 
-		arr[1] = new Student(1,"Arman -"+" Mark-95"); 			 // initialize the first elements of the array
-		arr[2] = new Student(2,"Robi  -"+" Mark-92");				 // initialize the second elements of the array
-		arr[3] = new Student(3,"Shikar-"+" Mark-80");  			 // so on...
-		arr[4] = new Student(4,"Ruben -"+" Mark-78");
-		arr[5] = new Student(5,"Mohit -"+" Mark-70");
+		arr[1] = new Student(1,"Arman -"+"Mark-",92); 			 // initialize the first elements of the array
+		arr[2] = new Student(2,"Robi  -"+" Mark-",90);				 // initialize the second elements of the array
+		arr[3] = new Student(3,"Shikar-"+" Mark-",80);  			 // so on...
+		arr[4] = new Student(4,"Ruben -"+" Mark-",78);
+		arr[5] = new Student(5,"Mohit -"+" Mark-",70);
 
-		for (int i = 1; i <arr.length; i++)	           // accessing the elements of the specified array
+		for (int i = 1; i <arr.length; i++)	           			// accessing the elements of the specified array
 			System.out.println("Serial No " + i + " : Roll - " +
-					arr[i].roll_no +" "+ arr[i].name);
+					arr[i].roll_no +" "+ arr[i].name+ ""+arr[i].mark);
 	}
 }
