093b6c71ffd46919fb74546de548da5344b09471
Mosarrof Hossain <phmosharaf@gmail.com>
2018-08-17 02:47:03 -0400

    Clone array added

---
+++ src/exampleconstructors/TestClone.java
@@ -0,0 +1,21 @@
+package exampleconstructors;
+// Java program to demonstrate 
+// cloning of one-dimensional arrays
+
+class TestClone
+{ 
+	public static void main(String args[]) 
+	{
+		int intArray[] = {1,2,3};
+		
+		int cloneArray[] = intArray.clone();
+		
+		// will print false as deep copy is created
+		// for one-dimensional array
+		System.out.println(intArray == cloneArray);
+		
+		for (int i = 0; i < cloneArray.length; i++) {
+			System.out.print(cloneArray[i]+" ");
+		}
+	}
+}
