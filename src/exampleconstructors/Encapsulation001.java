730f45a4e78800168426e7c0ceeb20139681b2ea
Mosarrof Hossain <phmosharaf@gmail.com>
2018-08-18 14:17:48 -0400

    Branch

---
+++ src/exampleconstructors/Encapsulation001.java
@@ -0,0 +1,35 @@
+package exampleconstructors;
+
+public class Encapsulation001 {
+
+
+	   private String name;
+	   private String idNum;
+	   private int age;
+// commment ....
+	   public int getAge() {
+	      return age;
+	   }
+
+	   public String getName() {
+	      return name;
+	   }
+
+	   public String getIdNum() {
+	      return idNum;
+	   }
+
+	   public void setAge( int newAge) {
+	      age = newAge;
+	   }
+
+	   public void setName(String newName) {
+	      name = newName;
+	   }
+
+	   public void setIdNum( String newId) {
+		      idNum = newId;
+	   }
+	}
+		
+
